1aaa05943aa9396f41d7cffa027b18ad815d59b2
dhakalsandesh0 <dhakalsandesh0@gmail.com>
2022-08-12 10:08:39 +0545

    flash messages

--- app/controllers/SignupController.php
+++ app/controllers/SignupController.php
@@ -9,9 +9,7 @@ class SignupController extends ControllerBase{
 
     public function registerAction()
     {
-        
         $user = new Users();
-
         // Store and check for errors
         $success = $user->save(
             $this->request->getPost(),
@@ -20,19 +18,17 @@ class SignupController extends ControllerBase{
                 "email",
             ]
         );
-
         if ($success) {
-            echo "Thanks for registering!";
+            // echo "Thanks for registering!";
+            $this->flash->success("The user is registered.");
         } else {
-            echo "Sorry, the following problems were generated: ";
-
+            // echo "Sorry, the following problems were generated: ";
+            $this->flash->error("Sorry, the following problems were generated.");
             $messages = $user->getMessages();
-
             foreach ($messages as $message) {
                 echo $message->getMessage(), "<br/>";
             }
         }
-
         $this->view->disable();
     }
     }
